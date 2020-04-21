583124bac60707a21983e1c020de77ec9a33387b
infam0us94 <ivanpopoff94@mail.ru>
2020-04-21 16:50:26 +0300

    third commit

---
+++ src/com/company/Task_10.java
@@ -0,0 +1,85 @@
+package com.company;
+
+//Задача о рюкзаке
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Task_10 {
+
+    public static void main(String args[]) {
+
+        final Item item1 = new Item(4, 20);
+        final Item item2 = new Item(3, 18);
+        final Item item3 = new Item(2, 14);
+
+        final Item[] items = {item1, item2, item3};
+
+        Arrays.sort(items, Comparator.comparingDouble(Item::valuePerUnitOfWeight).reversed());
+
+        System.out.println(Arrays.toString(items));
+
+        final int W = 7;
+
+        int weightSoFar = 0;
+        double valueSoFar = 0;
+        int currentItem = 0;
+
+        while (currentItem < items.length && weightSoFar != W) {
+            if (weightSoFar + items[currentItem].getWeight() < W) {
+                //берем объект целиком
+
+                valueSoFar += items[currentItem].getValue();
+                weightSoFar += items[currentItem].getWeight();
+
+            } else {
+                //берем только частями
+                valueSoFar += ((W - weightSoFar) / (double) items[currentItem].getWeight()) * items[currentItem].getValue();
+
+                weightSoFar = W;
+            }
+            currentItem++;
+        }
+        System.out.println("Ценность наилучшего набора: " + valueSoFar);
+    }
+
+
+}
+
+class Item {
+    private int weight;
+    private int value;
+
+    public Item(int weight, int value) {
+        this.weight = weight;
+        this.value = value;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public double valuePerUnitOfWeight() {
+        return value / (double) weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "w=" + weight +
+                ",v=" + value +
+                '}';
+    }
+}
